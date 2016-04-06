138e9eb7df6b9f435d670adbe93eefbe418f5197
dalexander15 <daniel.alexander15@hotmail.com>
2016-04-06 14:03:22 -0300

    game status singleton created. needs to be used in game ref and game logic;

--- src/GameStatus.java
+++ src/GameStatus.java
@@ -2,13 +2,40 @@
  * Created by DanDan on 2016-04-06.
  */
 public class GameStatus {
+
     private boolean playerWon;
     private boolean computerWon;
     private boolean tie;
+    private static GameStatus singleton = null;
 
-    public GameStatus (boolean playerCheck, boolean compCheck, boolean tieCheck){
-        playerWon = playerCheck;
-        computerWon = compCheck;
-        tie =
+    private GameStatus (){
+
     }
+
+    public static GameStatus getInstance(){
+        if (singleton == null){
+            singleton = new GameStatus();
+        }
+        return singleton;
+    }
+
+    public void setPlayerWon(boolean in){
+        playerWon = in;
+    }
+    public void setComputerWon(boolean in){
+        computerWon = in;
+    }
+    public void setTie(boolean in){
+        tie = in;
+    }
+    public boolean getPlayerWon(){
+        return playerWon;
+    }
+    public boolean getComputerWon(){
+        return computerWon;
+    }
+    public boolean getTie(){
+        return tie;
+    }
+
 }
